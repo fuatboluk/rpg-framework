da4c8f02dc276e3426645b019343d4f5cd7e57b8
Fuat BÖLÜK <mektup@fuatboluk.com.tr>
2025-02-22 01:25:38 +0300

    create/verify password

--- system/security.php
+++ system/security.php
@@ -8,4 +8,14 @@ class security
         return str_replace(['"', "'", "<", ">", '\\', "/"], '', $data);
     }
 
+    public function create_password($password)
+    {
+        return bin2hex(password_hash($password, PASSWORD_BCRYPT));
+    }
+
+    public function verify_password($password, $hash)
+    {
+        return (password_verify($password, hex2bin($hash)) ? 1 : 0);
+    }
+
 }
