184cebb5f7ef4e1d767b5688bf8516f243723039
Fuat BÖLÜK <mektup@fuatboluk.com.tr>
2025-02-22 01:29:16 +0300

    Added user model to root controller

--- app/root/controller.php
+++ app/root/controller.php
@@ -8,12 +8,15 @@ class controller extends rpg
 
     protected $session;
 
+    protected $user;
+
     public function __construct()
     {
         $this->session = $this->module("session");
         $this->id = session_id();
 
         $this->session->control($this->id);
+        $this->user = $this->model("user");
     }
     */
 
