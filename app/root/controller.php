f0b105f0499cc6305ac1b2b55a36c6a51ffcc878
Fuat BÖLÜK <mektup@fuatboluk.com.tr>
2025-02-21 13:27:54 +0300

    Added root session checking

--- app/root/controller.php
+++ app/root/controller.php
@@ -3,6 +3,18 @@
 class controller extends rpg
 {
 
-    // Root Controller
+    /*
+    protected $id;
+
+    protected $session;
+
+    public function __construct()
+    {
+        $this->session = $this->module("session");
+        $this->id = session_id();
+
+        $this->session->control($this->id);
+    }
+    */
 
 }
