367a33185e8ca6632fe580f0ed3379d4a50d1b82
Fuat BÖLÜK <mektup@fuatboluk.com.tr>
2025-03-11 16:51:41 +0300

    Update user.php

--- app/models/user.php
+++ app/models/user.php
@@ -13,9 +13,21 @@ class user extends model
         return ($this->update("users", [$column => $value], ["email" => $email]) ? 1 : 0);
     }
 
-    public function info($email)
+    public function check($username)
     {
-        return $this->where("email", "=", $email)->select("users");
+        return ($this->where("username", "=", $username)->select("users")->username ? 1 : 0);
+    }
+
+    public function info($data)
+    {
+        if (stripos($data, "@") == true)
+        {
+            return $this->where("email", "=", $data)->select("users");
+        }
+        else
+        {
+            return $this->where("username", "=", $data)->select("users");
+        }
     }
 
     public function remove($email)
@@ -35,6 +47,16 @@ class user extends model
         }
     }
 
-    // register
+    public function register($email, $password)
+    {
+        $array = [
+            "username" => "user".rand(111111, 999999),
+            "email" => $email,
+            "password" => security::create_password($password),
+            "register_date" => settings::$date." ".settings::$time
+        ];
+
+        return ($this->insert("users", $array) ? 1 : 0);
+    }
     
 }
